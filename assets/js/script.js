18337c30063428098f3fbaa8421d70b4fac38766
Pavel Vais <vaispavel@gmail.com>
2022-02-24 07:31:43 +0100

    Aktualizace informaci, optimalizace fontu (uklid)

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,4 +19,31 @@ $(document).ready(function () {
     }, true)
 
 
+    $("#rsvp-form").submit(function () {
+        $.post($(this).attr("action"), $(this).serializeObject(), function (data) {
+            $("#rsvp-form").slideUp();
+            $("#rsvp-form-message").slideDown();
+        }, "json")
+            .catch(function () {
+                $("#rsvp-form").slideUp();
+                $("#rsvp-form-message").slideDown();
+            });
+        return false;
+    })
 })
+
+$.fn.serializeObject = function () {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function () {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
